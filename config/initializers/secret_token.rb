3e39c5f3003db5a289e14a0b1a2b10474949c004
Sarup Banskota <sbanskota08@gmail.com>
2013-12-31 21:37:53 +0530

    Add rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinadarshica::Application.config.secret_token = '28f2559cedda57841584946a6324b0ed7ab4052de9f494b9e18337ca941ef83776ffd501b79c12a4ca0f3c719103d8d5556faa57805bcef23354db0aa2e415f3'
